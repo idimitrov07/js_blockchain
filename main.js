d8eb47bdecedefac06a277d52e8042d26dc5315c
Ivan Dimitrov <idimitrov07@gmail.com>
2018-03-27 13:47:54 +0300

    Create block class, import npm modules

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const SHA256 = require("crypto-js/sha256");
+
 class Block {
-  
+  constructor(index, timestamp, data, previuosHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previuosHash = previuosHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.previuosHash + this.timestamp + 
+                  JSON.stringify(this.data)).toString();
+  }
 }
